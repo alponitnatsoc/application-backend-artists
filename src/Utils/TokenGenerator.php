d337bca4e68a0407909cf33546d9e6cc0ddda3a2
Andres Felipe Ramirez <alponitnatsnoc@gmail.com>
2018-10-03 01:55:08 -0500

    Testing token generator for ORM custom generator

--- src/Utils/TokenGenerator.php
+++ src/Utils/TokenGenerator.php
@@ -3,13 +3,34 @@
 namespace App\Utils;
 
 use RandomLib\Factory;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Id\AbstractIdGenerator;
 
-class TokenGenerator
+class TokenGenerator extends AbstractIdGenerator
 {
     const LENGTH = 16;
     const VOCAB = 'ABCDEFGHIJKLMNOPQRSTUVXYZ0123456789';
+    const MAX_ATTEMPTS = 10000;
 
-    public static function generate(int $length = self::LENGTH, string $vocab = self::VOCAB): string
+    public function generate(EntityManager $em, $entity) {
+        $entity_name = $em->getClassMetadata(get_class($entity))->getName();
+        $attempt = 0;
+        while (true) {
+            $token = self::generateToken(6, self::VOCAB);
+            $item = $em->find($entity_name, $token);
+
+            if (!$item) {
+                return $token;
+            }
+
+            $attempt++;
+            if ($attempt > self::MAX_ATTEMPTS) {
+                throw new \Exception('TokenGenerator tried hardly 10000 times, but failed to generate a new unique token.');
+            }
+        }
+    }
+
+    public static function generateToken(int $length = self::LENGTH, string $vocab = self::VOCAB): string
     {
         $generator = (new Factory())->getMediumStrengthGenerator();
 
