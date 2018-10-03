8e018c6f3ba528f546c110106dcf1b2626489f03
Andres Felipe Ramirez <alponitnatsnoc@gmail.com>
2018-10-03 02:19:04 -0500

    creating showAlbum logic with json response

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -14,6 +14,19 @@ class AlbumController extends AbstractController
      */
     public function showAlbum($token) {
         $response = new Response();
+        $em = $this->getDoctrine()->getManager();
+        $albumsRepo = $em->getRepository("App:Album");
+        $album = $albumsRepo->find($token);
+        $artistArr =[];
+        $songArr = [];
+        foreach ($album->getArtists() as $akey => $artist){
+            $artistArr[$akey] = ['name' => $artist->getName(), 'token' => $artist->getToken()];
+        }
+        foreach ($album->getSongs() as $skey => $song){
+            $songArr[$skey] = ['title' => $song->getTitle(), 'length' => $song->getLengthinSeconds()];
+        }
+        $data['album'] = ['title' => $album->getName(), 'token' => $album->getToken(),'cover' => $album->getCover(), 'description' => $album->getDescription(), 'artists' => $artistArr, 'songs' => $songArr];
+        $response->setContent(json_encode($data));
         $response->setContent(json_encode());
         $response->headers->set('Content-Type','application/json');
         $response->setStatusCode(StatusCode::OK);
